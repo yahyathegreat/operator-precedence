90b83f242424b436ab14c8c8e2e9c40eb90835f6
yahyathegreat <yahyamardan1234@gmail.com>
2024-09-27 19:56:57 +0500

    something precedence

---
+++ main.py
@@ -0,0 +1,13 @@
+v = 4
+w = 5
+x = 8
+y = 2
+z = 0
+z = (v+w) * x/ y;
+print("value of (v+w) * x/ y is ",  z)
+name = "alex"
+age= 0
+if name == "alex" or name == "john" and age >= 2 :
+    print("hello welcome")
+else :
+    print("good bye")
